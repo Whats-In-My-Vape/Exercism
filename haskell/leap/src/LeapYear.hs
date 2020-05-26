c7253c8bf5cb1d8f5d491bccecc6c49f90f2a92c
Kevin Power <kevin.power.97.kp@gmail.com>
2020-05-26 05:50:56 +0100

    Revert "init setup"
    This reverts commit d2d7f0228ccaaaf786027e7b6d0345b23d0ffb00.

--- haskell/leap/src/LeapYear.hs
+++
@@ -1,9 +0,0 @@
-module LeapYear (isLeapYear) where
-
-isLeapYear :: Integer -> Bool
-isLeapYear year 
-    | helper 400 = True
-    | helper 100 = False
-    | helper 4 = True
-    | otherwise = False
-    where helper x = year `mod` x == 0
