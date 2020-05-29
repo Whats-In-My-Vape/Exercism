31552e6ce7bc5d6db0e24b3dd7d8954cec30bac3
Kevin Power <kevin.power.97.kp@gmail.com>
2020-05-29 11:50:28 +0100

    sufice

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -2,8 +2,8 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
 isLeapYear year 
-    | helper 400 = True
-    | helper 100 = False
-    | helper 4 = True
+    | isYearDivisible 400 = True
+    | isYearDivisible 100 = False
+    | isYearDivisible 4 = True
     | otherwise = False
-    where helper x = year `mod` x == 0
+    where isYearDivisible x = year `mod` x == 0
