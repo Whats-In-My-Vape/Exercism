3e6948c6aac0b828cb2728ab2bd917483c515ca1
Kevin Power <kevin.power.97.kp@gmail.com>
2020-07-01 01:13:22 +0100

    leap

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,9 +1,11 @@
 module LeapYear (isLeapYear) where
 
+-- swap rem in instead of doing that where recursion
+-- switch bool values due to this
+
 isLeapYear :: Integer -> Bool
 isLeapYear year 
-    | isYearDivisible 400 = True
-    | isYearDivisible 100 = False
-    | isYearDivisible 4 = True
-    | otherwise = False
-    where isYearDivisible x = year `rem` x == 0
+    |  rem year 4 /= 0 = False
+    |  rem year 400 == 0 = True
+    |  rem year 100 == 0 = False
+    |  otherwise = True
